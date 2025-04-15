b1a976f35c6d1ac04e7e345488f371677ad50f1a
anhnt160190 <anh.nt160190@gmail.com>
2025-04-16 06:14:12 +0700

    fix: use tailwind cdn

--- web-admin/vite.config.ts
+++ web-admin/vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import federation from '@originjs/vite-plugin-federation';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     federation({
       name: 'web-admin',
       filename: 'remoteEntry.js',
