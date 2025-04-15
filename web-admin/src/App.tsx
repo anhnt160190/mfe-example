ab9a4cd05fb40e5141db341197f5c7035286bf28
anhnt160190 <anh.nt160190@gmail.com>
2025-04-15 07:09:20 +0700

    feat: init web-admin

---
+++ web-admin/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+import LoginPage from './pages/auth/Login';
+import NotFoundPage from './pages/not_found/404';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<NotFoundPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
